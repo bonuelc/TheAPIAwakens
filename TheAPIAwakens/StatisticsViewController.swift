72295c30c2207334182a7a8184e311e29553d8f1
bonuelc <bonuelc@gmail.com>
2016-09-30 09:52:37 -0600

    Set up root view controller

---
+++ TheAPIAwakens/StatisticsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  StatisticsViewController.swift
+//  TheAPIAwakens
+//
+//  Created by Christopher Bonuel on 9/30/16.
+//  Copyright © 2016 Christopher Bonuel. All rights reserved.
+//
+
+import UIKit
+
+class StatisticsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
