af823a6d014dcdd75018a34de60639e84362bb6a
bonuelc <bonuelc@gmail.com>
2016-09-30 14:59:59 -0600

    Connect IBOutlets to UIButtons

--- TheAPIAwakens/ViewController.swift
+++ TheAPIAwakens/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var charactersButton: UIButton!
+    @IBOutlet weak var vehiclesButton: UIButton!
+    @IBOutlet weak var starshipsButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
