4d63feb7d662ccd5b4c1776c8ea5bce860404afb
bonuelc <bonuelc@gmail.com>
2016-09-30 13:14:38 -0600

    Add blank SWAPIClient.swift

---
+++ TheAPIAwakens/SWAPIClient.swift
@@ -0,0 +1,9 @@
+//
+//  SWAPIClient.swift
+//  TheAPIAwakens
+//
+//  Created by Christopher Bonuel on 9/30/16.
+//  Copyright © 2016 Christopher Bonuel. All rights reserved.
+//
+
+import Foundation
