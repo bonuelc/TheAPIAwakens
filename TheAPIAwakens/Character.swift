51b84d3a5842624cb7ee9223cf147bad692b679c
bonuelc <bonuelc@gmail.com>
2016-09-30 15:34:27 -0600

    Implement Character struct

---
+++ TheAPIAwakens/Character.swift
@@ -0,0 +1,18 @@
+//
+//  Character.swift
+//  TheAPIAwakens
+//
+//  Created by Christopher Bonuel on 9/30/16.
+//  Copyright © 2016 Christopher Bonuel. All rights reserved.
+//
+
+import Foundation
+
+struct Character {
+    let name: String
+    let born: String
+    let home: String
+    let height: String
+    let eyes: String
+    let hair: String
+}
