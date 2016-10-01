21fb87f0fb41fa1ac8075fce9c0ff16c816363e4
bonuelc <bonuelc@gmail.com>
2016-10-01 09:38:03 -0600

    Implement Vehicle struct

---
+++ TheAPIAwakens/Vehicle.swift
@@ -0,0 +1,18 @@
+//
+//  Vehicle.swift
+//  TheAPIAwakens
+//
+//  Created by Christopher Bonuel on 10/1/16.
+//  Copyright © 2016 Christopher Bonuel. All rights reserved.
+//
+
+import Foundation
+
+struct Vehicle {
+    let name: String
+    let make: String
+    let cost: String
+    let length: String
+    let `class`: String
+    let crew: String
+}
