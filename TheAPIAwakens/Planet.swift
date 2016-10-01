7dad7ad19b4a982b1aefac0c9625e928a747b6df
bonuelc <bonuelc@gmail.com>
2016-10-01 10:03:25 -0600

    Implement Planet struct
    Needed to get name from Character.homeworld

---
+++ TheAPIAwakens/Planet.swift
@@ -0,0 +1,14 @@
+//
+//  Planet.swift
+//  TheAPIAwakens
+//
+//  Created by Christopher Bonuel on 10/1/16.
+//  Copyright © 2016 Christopher Bonuel. All rights reserved.
+//
+
+import Foundation
+
+struct Planet {
+    let name: String
+    let diameter: String
+}
