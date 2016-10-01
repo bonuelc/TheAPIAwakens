a2ee8f2e822fa196307739033a624a0ccb4cb6c0
bonuelc <bonuelc@gmail.com>
2016-10-01 09:42:50 -0600

    Implement Starship class

---
+++ TheAPIAwakens/Starship.swift
@@ -0,0 +1,18 @@
+//
+//  Starship.swift
+//  TheAPIAwakens
+//
+//  Created by Christopher Bonuel on 10/1/16.
+//  Copyright © 2016 Christopher Bonuel. All rights reserved.
+//
+
+import Foundation
+
+struct Starship {
+    let name: String
+    let make: String
+    let cost: String
+    let length: String
+    let `class`: String
+    let crew: String
+}
