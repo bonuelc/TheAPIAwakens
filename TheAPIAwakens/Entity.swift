ec10818d00788ec45e9b90be8bc92ca41d17aef4
bonuelc <bonuelc@gmail.com>
2016-10-03 15:34:17 -0600

    Implement Entity protocol

---
+++ TheAPIAwakens/Entity.swift
@@ -0,0 +1,12 @@
+//
+//  Entity.swift
+//  TheAPIAwakens
+//
+//  Created by Christopher Bonuel on 10/3/16.
+//  Copyright © 2016 Christopher Bonuel. All rights reserved.
+//
+
+protocol Entity {
+    var name: String { get }
+    var size: Double? { get }
+}
