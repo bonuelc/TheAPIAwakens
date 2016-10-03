24c3e48405bd58cfd9868e2bbf307b14bf632bf6
bonuelc <bonuelc@gmail.com>
2016-10-03 15:57:47 -0600

    Implement prepareForSegue
    Set entityTypePicked property so statisticsVC will know
    what data to fetch

--- TheAPIAwakens/ViewController.swift
+++ TheAPIAwakens/ViewController.swift
@@ -21,5 +21,19 @@ class ViewController: UIViewController {
     override func viewWillDisappear(animated: Bool) {
         self.navigationController?.setNavigationBarHidden(false, animated: true)
     }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+        guard let statisticsVC = segue.destinationViewController as? StatisticsViewController, sender = sender as? UIButton else {
+            return
+        }
+        
+        switch sender {
+        case charactersButton: statisticsVC.entityTypePicked = .Characters
+        case vehiclesButton: statisticsVC.entityTypePicked = .Vehicles
+        case starshipsButton: statisticsVC.entityTypePicked = .Starships
+        default: fatalError()
+        }
+    }
 }
 
