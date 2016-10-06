19e2b30215e6fbbc64365bdfb0a88e6780bb4d50
bonuelc <bonuelc@gmail.com>
2016-10-06 13:45:51 -0600

    Implement ShowcaseTableView class
    SQUASH

---
+++ TheAPIAwakens/ShowcaseTableView.swift
@@ -0,0 +1,14 @@
+//
+//  ShowcaseTableView.swift
+//  TheAPIAwakens
+//
+//  Created by Christopher Bonuel on 10/6/16.
+//  Copyright © 2016 Christopher Bonuel. All rights reserved.
+//
+
+import UIKit
+
+class ShowcaseTableView: UITableView {
+    var smallest: Entity?
+    var largest: Entity?
+}
