50aeda0a3cf592240a7cc9905344d9caacf8e647
bonuelc <bonuelc@gmail.com>
2016-10-08 09:31:58 -0600

    Implement StatisticTableViewCell class with IBOutlets

---
+++ TheAPIAwakens/StatisticTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  StatisticTableViewCell.swift
+//  TheAPIAwakens
+//
+//  Created by Christopher Bonuel on 10/5/16.
+//  Copyright © 2016 Christopher Bonuel. All rights reserved.
+//
+
+import UIKit
+
+class StatisticTableViewCell: UITableViewCell {
+    @IBOutlet weak var keyLabel: UILabel!
+    @IBOutlet weak var valueLabel: UILabel!
+    @IBOutlet weak var unitsSegmentedControl: UISegmentedControl!
+}
