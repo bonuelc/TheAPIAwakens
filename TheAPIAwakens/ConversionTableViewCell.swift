e7fc9d1628e87826fa861abe96296b20086e8546
bonuelc <bonuelc@gmail.com>
2016-10-12 09:38:29 -0600

    Add ConversionTableViewCell to MemberStatisticsTableView

---
+++ TheAPIAwakens/ConversionTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ConversionTableViewCell.swift
+//  TheAPIAwakens
+//
+//  Created by Christopher Bonuel on 10/11/16.
+//  Copyright © 2016 Christopher Bonuel. All rights reserved.
+//
+
+import UIKit
+
+class ConversionTableViewCell: UITableViewCell {
+    @IBOutlet weak var decimalPadTextField: UITextField!
+}
