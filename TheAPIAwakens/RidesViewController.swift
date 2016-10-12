445ddf2205d6ce81f1391ab537d5d10aec4ccf09
bonuelc <bonuelc@gmail.com>
2016-10-12 15:22:31 -0600

    Add RidesViewController

---
+++ TheAPIAwakens/RidesViewController.swift
@@ -0,0 +1,16 @@
+//
+//  RidesViewController.swift
+//  TheAPIAwakens
+//
+//  Created by Christopher Bonuel on 10/12/16.
+//  Copyright © 2016 Christopher Bonuel. All rights reserved.
+//
+
+import UIKit
+
+class RidesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
