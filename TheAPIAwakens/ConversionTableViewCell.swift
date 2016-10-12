442e62da70fee40bf3558dc9aa6ba8868f1c398f
bonuelc <bonuelc@gmail.com>
2016-10-12 10:38:36 -0600

    Pass exchangeRate from ConversionTableViewCell

--- TheAPIAwakens/ConversionTableViewCell.swift
+++ TheAPIAwakens/ConversionTableViewCell.swift
@@ -9,8 +9,33 @@
 import UIKit
 
 class ConversionTableViewCell: UITableViewCell {
+    
+    var lastValue: Double = 1.01
 
     var delegate: ExchangeRateDelegate!
     
     @IBOutlet weak var decimalPadTextField: UITextField!
+    
+    override func awakeFromNib() {
+        
+        super.awakeFromNib()
+        
+        revertExchangeRate()
+        
+        setExchangeRate()
+    }
+    
+    func setExchangeRate() {
+        
+        guard var delegate = delegate, let exchangeRateString = decimalPadTextField.text, exchangeRate = Double(exchangeRateString) else {
+            revertExchangeRate()
+            return
+        }
+        
+        delegate.exchangeRate = exchangeRate
+    }
+    
+    func revertExchangeRate() {
+        decimalPadTextField.text = "\(lastValue)"
+    }
 }
